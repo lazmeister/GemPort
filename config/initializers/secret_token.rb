9090e7c9898e37f81b70c7f9788044c2b6285478
Laszlo Alt <laszlo.alt@gmail.com>
2013-03-08 21:29:55 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemPort::Application.config.secret_token = 'c9a2b1e6ba74739b090a5f56a51ddd9ed30c8dcd0957938ad57860386230ff397adf620a26e072b9228dff719117abedb53b374bbabe8c6153445f814b7d497a'
