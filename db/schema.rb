dc71df6801634b4ebb300b84ab13aa82073db941
Laszlo Alt <laszlo.alt@gmail.com>
2013-04-01 19:27:17 -0400

    fixed up Facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329173015) do
+ActiveRecord::Schema.define(:version => 20130401195453) do
 
   create_table "referrals", :force => true do |t|
     t.string   "description"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(:version => 20130329173015) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
