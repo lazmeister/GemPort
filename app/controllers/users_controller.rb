eb19ad499588ac75ca872fb62bfcd367d178d2ec
Laszlo Alt <laszlo.alt@gmail.com>
2013-05-23 16:42:49 -0400

    update registration values

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @referrals = @user.referrals
   end
+  def index
+    @client_city = request.location.city
+    @client_country = request.location.country
+  end
 end
